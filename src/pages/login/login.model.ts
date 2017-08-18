cdbbf6d26be7f9eacfea170b54dd6db07e56ea45
secretmoo35 <sarayut-moo.th@hotmail.com>
2017-08-18 16:39:23 +0700

    login mick

--- src/pages/login/login.model.ts
+++ src/pages/login/login.model.ts
@@ -1,10 +1,6 @@
-//Mick Created 18/8/60 12.50 pm.
+
 export class UserModel {
-    userdata: Array<UserItem>;
-}
-export class UserItem {
     _id: string;
-    created: string;
     displayName: string;
     email: string;
     firstName: string;
@@ -12,7 +8,4 @@ export class UserItem {
     username: string;
     password: string;
     profileImageURL: string;
-    provider: string;
-    roles: Array<any>;
 }
-
