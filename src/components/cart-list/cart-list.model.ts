6b95c1e7b02f73df7ba0e4e80394dddca7d31312
Cyber-Mac-17 <cyber-mac-17@MacBook-Pro-17.local>
2017-08-18 12:18:18 +0700

    add component cart-list

---
+++ src/components/cart-list/cart-list.model.ts
@@ -0,0 +1,8 @@
+export class DataCartList {
+    id: string;
+    name: string;
+    detail: string;
+    price: number;
+    quantity: number;
+    imgUrl: string;
+}
--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { DataScrollXComponent } from './data-scroll-x/data-scroll-x';
 import { DataGridComponent } from './data-grid/data-grid';
 import { TopbarComponent } from './topbar/topbar';
+import { CartListComponent } from './cart-list/cart-list';
 @NgModule({
 	declarations: [DataScrollXComponent,
     DataGridComponent,
-    TopbarComponent],
+    TopbarComponent,
+    CartListComponent],
 	imports: [],
 	exports: [DataScrollXComponent,
     DataGridComponent,
-    TopbarComponent]
+    TopbarComponent,
+    CartListComponent]
 })
 export class ComponentsModule {}
